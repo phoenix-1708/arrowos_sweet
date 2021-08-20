c744e141b6dac07d9517999fcb0a3c6bda1d6daa
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2021-08-20 17:07:11 +0530

    Update kernel_setup.sh

--- kernel_setup.sh
+++ kernel_setup.sh
@@ -14,11 +14,15 @@ AK_REPO="https://github.com/phoenix-1708/AnyKernel3"
 AK_BRANCH="sweet"
 AK_DIR="$HOME/AnyKernel3"
 TC_DIR="$HOME/proton-clang"
+TC_DIR64="$HOME/gcc"
+TC_DIR32="$HOME/gcc32"
 # End Config
 
 # clone_tc - clones proton clang to TC_DIR
 clone_tc() {
 	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git $TC_DIR
+	git clone --depth=1 git clone https://github.com/sohamxda7/llvm-stable -b gcc64 $TC_DIR64
+	git clone --depth=1 git clone https://github.com/sohamxda7/llvm-stable -b gcc32 $TC_DIR32
 }
 
 # Clones anykernel
